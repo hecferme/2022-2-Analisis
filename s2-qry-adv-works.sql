8568bbcd8f176090ca7283dfeae280c095eb9d0a
Héctor Fernández <hecferme@gmail.com>
2022-05-28 10:20:58 -0600

    s2-select

--- s2-qry-adv-works.sql
+++ s2-qry-adv-works.sql
@@ -1,6 +1,17 @@
 use AdventureWorksLT2019
-select pc.Name as CategoryName, p.ProductID, p.Name, p.Color, p.ListPrice from SalesLT.Product as p
+select  pm.Name as ProductModel, ppc.Name as ParentProductCategory, pc.Name as CategoryName, p.ProductID, p.Name, p.Color, p.ListPrice from SalesLT.Product as p
 inner join SalesLT.ProductCategory pc
 	on pc.ProductCategoryID = p.ProductCategoryID
+inner join SalesLT.ProductCategory ppc
+	on ppc.ProductCategoryID = pc.ParentProductCategoryID
+inner join SalesLT.ProductModel pm
+	on pm.ProductModelID = p.ProductModelID
 where p.Color like '%e'
 order by p.ListPrice desc
+
+
+/*
+tarea moral:
+con base en el url http://bifuture.blogspot.com/2010/04/datavault-identying-hubs.html
+defina los campos que deben tener las dimensiones (product, customer) y las fact (sod, soh)
+*/
